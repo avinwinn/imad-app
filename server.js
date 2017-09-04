e0a478861c146bab4d673b30a701d8750272776c
avinwinn <30404877+avinwinn@users.noreply.github.com>
2017-09-04 12:27:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var path = require('path');
 
 var Pool = require('pg').Pool;
 
+var config=
+{
+    user:'avinnagre',
+    database:'avinnagre',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.emv.DB_PASSWORD,
+}
+
 
 var app = express();
 app.use(morgan('combined'));
